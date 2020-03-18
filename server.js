b1a549676f0b612bb0ebbc54c90402b574301c65
Doni Setiawan <donisetiawan.050@gmail.com>
2020-03-18 09:56:16 +0700

    The bootstrapping phase

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
+
+import { connect } from './lib/db';
 
 const app = express();
-const port = 3000;
+
+const config = require('./config.json')[app.get('env')];
+
+connect(config.mongoUrl);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(
+  methodOverride((req, res) => {
+    if (
+      req.body
+      && typeof req.body === 'object'
+      && '_method' in req.body
+    ) {
+      // look in urlencoded POST bodies and delete it
+      const method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  }),
+);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(config.port, () => console.log(
+  `Example app listening on port ${config.port}!`,
+));
